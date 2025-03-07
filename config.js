fa61405bc097620e2dc251a01dcd9cd45a259ad5
Suresh <suresh_v001@seyone.co>
2025-03-07 15:08:31 +0530

    changes

--- config.js
+++ config.js
@@ -9,6 +9,8 @@ const {
   AWS_SECRET_KEY_ACCESS,
   AWS_BUCKET,
   AWS_REGION,
+  ZOOM_API_KEY,
+  ZOOM_API_SECRET_KEY,
   SESSION_TOKEN,
   REFRESH_TOKEN,
 } = process.env;
@@ -18,6 +20,12 @@ export const awsRegion = AWS_REGION || "ap-south-1";
 export const awsAccessKeyId = AWS_KEY_ID;
 export const awsSecretAccessKey = AWS_SECRET_KEY_ACCESS;
 export const awsBucketName = AWS_BUCKET;
+
+// Zoom Integration Testing
+
+export const APIKey = ZOOM_API_KEY;
+
+export const APISecret = ZOOM_API_SECRET_KEY;
 
 //port
 export const port = PORT;
