4032c417f19205521cc3a4aa4e3cb1accca7a5d5
Suresh <suresh_v001@seyone.co>
2025-03-07 15:00:33 +0530

    Update email.js

--- utils/email.js
+++ utils/email.js
@@ -1,5 +1,5 @@
 import sgMail from "@sendgrid/mail";
-sgMail.setApiKey("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");
+sgMail.setApiKey("");
 
 export function sendMail(msg) {
   sgMail.send(msg).then((res) => {});
